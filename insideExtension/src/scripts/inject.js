516fc460d062a5d7388d077edf54dcd2fa37f168
Xenotime <sandeep@gmail.com>
2018-02-02 14:29:08 +0530

    fix

--- insideExtension/src/scripts/inject.js
+++ insideExtension/src/scripts/inject.js
@@ -4,6 +4,6 @@ var EnhanceLibIsLoaded = true;
     var e = document.createElement('script');
     e.setAttribute('id', 'sb-script');
     e.setAttribute('mode', 'extension');
-    e.setAttribute('src', 'https://xenotime-india.github.io/force.com-chrome-ext/outsideExtension/build/scripts/onload.min.js');
+    e.setAttribute('src', 'https://xenotime-india.github.io/vf-page-builder/outsideExtension/build/scripts/onload.min.js');
     document.body.appendChild(e);
 })();
