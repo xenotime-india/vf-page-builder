6259c61d0e2da13e72bebf1dbf26bd67c4eb9ed3
Xenotime <sandeep@gmail.com>
2018-02-04 09:17:30 +0530

    fix

--- outsideExtension/src/scripts/api/index.js
+++ outsideExtension/src/scripts/api/index.js
@@ -9,5 +9,5 @@ export function fetchCustomObjects() {
 
 export function fetchPageLayout(Object) {
     const connection = sforce();
-    return connection.tooling.sobject(Object).describe();
+    return connection.metadata.read('CustomObject', [ Object ]);
 }
